c6acce1c61242c315b683c247bba269ebb1bd6f5
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-07-05 17:36:39 +0200

    feat: new utils header

--- framework/src/utils/utils.h
+++ framework/src/utils/utils.h
@@ -1,31 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libunit_launch.h                                   :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/05 16:25:43 by mweghofe          #+#    #+#             */
-/*   Updated: 2025/07/05 17:34:03 by mweghofe         ###   ########.fr       */
+/*   Created: 2025/07/05 17:34:30 by mweghofe          #+#    #+#             */
+/*   Updated: 2025/07/05 17:35:12 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBUNIT_LAUNCH_H
-# define LIBUNIT_LAUNCH_H
+#ifndef UTILS_H
+# define UTILS_H
 
-# include "t_libunit/t_unit_test.h"
-# include "t_libunit/t_libunit.h"
+#include "launcher/libunit_launch.h"
 
-typedef enum e_result
-{
-	R_OK,
-	R_KO,
-	R_SIGSEGV,
-	R_SIGBUS,
-	R_SIGOTHER,
-}	t_result;
-
-t_result	get_child_status(int status);
-void		prt_single_test_result(char *collection, t_unit_test *test);
+void	prt_total_stats(t_stats *stats, char *name);
 
 #endif

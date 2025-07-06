56da522cf935ac4af526da11861a3613b8f9c850
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-07-06 12:18:49 +0200

    feat: sigbus test

--- tests/mandatory/04_sigbus.c
+++ tests/mandatory/04_sigbus.c
@@ -3,14 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   04_sigbus.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/05 11:14:55 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/06 09:17:19 by ldulling         ###   ########.fr       */
+/*   Created: 2025/07/05 11:38:33 by mweghofe          #+#    #+#             */
+/*   Updated: 2025/07/06 12:18:06 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/mman.h>	// mmap()
+#include <fcntl.h>		// open(), close(), unlink()
+#include <unistd.h>		// truncate()
+
 int	sigbus(void)
 {
-	return (-1);
+	const char	*file;
+	char		*file_in_mem;
+	char		bus_err;
+	int			fd;
+	int			page_size;
+
+	file = "/tmp/SIGBUS_TRIGGER";
+	fd = open(file, O_CREAT | O_RDWR, 0644);
+	if (fd == -1)
+		return (-1);
+	page_size = getpagesize();
+	if (truncate(file, page_size * 3) == -1)
+		return (-1);
+	file_in_mem = mmap(NULL, page_size * 3, PROT_READ, MAP_SHARED, fd, 0);
+	if (close(fd) == -1)
+		return (-1);
+	if (truncate(file, page_size) == -1)
+		return (-1);
+	unlink(file);
+	bus_err = file_in_mem[page_size];
+	return (0);
 }

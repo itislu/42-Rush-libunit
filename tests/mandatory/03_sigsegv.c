cdd45742c7b5787b4d15cd5c86a25dcb497c6c8d
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-07-06 12:55:00 +0200

    reafactor: sigbus/fault returns

--- tests/mandatory/03_sigsegv.c
+++ tests/mandatory/03_sigsegv.c
@@ -6,7 +6,7 @@
 /*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 11:14:55 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/06 12:24:06 by mweghofe         ###   ########.fr       */
+/*   Updated: 2025/07/06 12:52:27 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@ int	sigsegv(void)
 	int			page_size;
 
 	file = "/tmp/SIGBUS_TRIGGER";
-	fd = open(file, O_CREAT | O_RDWR, 0644);
+	fd = open(file, O_CREAT | O_RDONLY, 0644);
 	if (fd == -1)
 		return (-1);
 	page_size = getpagesize();
 	if (truncate(file, page_size * 3) == -1)
-		return (-1);
+		return (close(fd), unlink(file), -1);
 	file_in_mem = mmap(NULL, page_size * 3, PROT_READ, MAP_SHARED, fd, 0);
 	if (close(fd) == -1)
-		return (-1);
+		return (unlink(file), -1);
 	if (truncate(file, page_size) == -1)
-		return (-1);
+		return (unlink(file), -1);
 	unlink(file);
 	bus_err = file_in_mem[(size_t) -1 / 2];
 	return (-1);

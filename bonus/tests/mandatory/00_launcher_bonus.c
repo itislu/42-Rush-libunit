31ad6c5fd94736cd9529f9a1dfb501eb86780ee8
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-07-06 16:42:26 +0200

    feat: timeout test

--- bonus/tests/mandatory/00_launcher_bonus.c
+++ bonus/tests/mandatory/00_launcher_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_launcher_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 09:06:24 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/06 15:57:59 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/06 16:36:50 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	mandatory_launcher(t_libunit *libunit)
 	libunit_load(libunit, "sigbus", sigbus);
 	libunit_load(libunit, "stack_overflow", stack_overflow);
 	libunit_load(libunit, "mmap_permissions", mmap_permissions);
+	libunit_load(libunit, "timeout", timeout);
 	libunit_launch(libunit);
 }
 // else
--- bonus/tests/mandatory/0x_timeout.c
+++ bonus/tests/mandatory/0x_timeout.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tests_bonus.h                                      :+:      :+:    :+:   */
+/*   0x_timeout.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/05 08:29:59 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/06 16:34:33 by mweghofe         ###   ########.fr       */
+/*   Created: 2025/07/06 16:34:12 by mweghofe          #+#    #+#             */
+/*   Updated: 2025/07/06 16:34:23 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TESTS_BONUS_H
-# define TESTS_BONUS_H
-
-int	ok(void);
-int	ko(void);
-int	sigsegv(void);
-int	sigbus(void);
-int	stack_overflow(void);
-int	mmap_permissions(void);
-int	timeout(void);
-
-#endif
+int	timeout(void)
+{
+	while (1)
+		;
+	return (0);
+}

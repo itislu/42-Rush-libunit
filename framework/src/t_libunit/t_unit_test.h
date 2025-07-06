3c97cd9cc8840cb2c7ebf76518af219864a2794d
Lukas Dullinger <itislu.git@gmail.com>
2025-07-06 22:50:31 +0200

    style: Norm mandatory framework

--- framework/src/t_libunit/t_unit_test.h
+++ framework/src/t_libunit/t_unit_test.h
@@ -6,14 +6,14 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 10:13:27 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/06 16:19:26 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/06 22:50:03 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef T_UNIT_TEST_H
 # define T_UNIT_TEST_H
 
-typedef struct s_unit_test	
+typedef struct s_unit_test
 {
 	const char	*name;
 	int			(*func)(void);
--- framework/src/t_libunit/unit_test_new.c
+++ framework/src/t_libunit/unit_test_new.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unit_test_new.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 22:49:41 by ldulling          #+#    #+#             */
+/*   Updated: 2025/07/06 22:50:08 by ldulling         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
 #include "t_unit_test.h"
 #include <stdbool.h>
@@ -5,7 +17,7 @@
 t_unit_test	*unit_test_new(const char *name, int (*func)(void))
 {
 	t_unit_test	*unit_test;
-	
+
 	unit_test = ft_calloc(1, sizeof(t_unit_test));
 	if (unit_test == NULL)
 		return (NULL);

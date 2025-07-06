1c73573bdd5bbcb501f900847b697d68dff7e0a0
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-07-06 16:22:17 +0200

    feat: print start new test series

--- bonus/framework/src/t_libunit/libunit_new_bonus.c
+++ bonus/framework/src/t_libunit/libunit_new_bonus.c
@@ -1,5 +1,6 @@
 #include "libft/ft_printf.h"
 #include "libft/libft.h"
+#include "utils/utils_bonus.h"
 #include "t_libunit_bonus.h"
 #include <unistd.h>
 
@@ -10,5 +11,6 @@ t_libunit	*libunit_new(void)
 	libunit = ft_calloc(1, sizeof(t_libunit));
 	if (libunit == NULL)
 		ft_dprintf(STDERR_FILENO, "ERROR: Failed to allocate libunit\n");
+	prt_test_start();
 	return (libunit);
 }

itislu/42-Rush-libunit

commit 7534be2aa2be2a2192b471fbf375ea83526b1927

feat: colored libunit output

framework/src/utils/prt_total_stats.c
@@ -1,37 +1,0 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   prt_total_stats.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/05 17:30:32 by mweghofe          #+#    #+#             */
-/*   Updated: 2025/07/05 21:20:59 by mweghofe         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft/ft_printf.h"
-#include "t_libunit/t_libunit.h"
-
-// prints all stats of t_stats, works with collection & total stats
-void	prt_total_stats(t_stats *stats, const char *name)
-{
-	ft_printf("\n%s :: total %i :: success %i :: fail %i :: crash %i\n\n",
-		name,
-		stats->n_tests,
-		stats->n_success,
-		stats->n_fail,
-		stats->n_crash);
-}
-
-/*
-
-	STRLEN : 01_sth : OK
-	STRLEN : 02_sth : KO
-	STRLEN : 03_sth : KO
-
-	STRLEN :: total tests 12 :: success 11 ...
-
-	SUMMARY :: total tests 235 :: success 200 ...
-
-*/

bonus/framework/src/launcher/prt_tests_result_bonus.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prt_tests_result_bonus_bonus.c                     :+:      :+:    :+:   */
+/*   prt_tests_result_bonus.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 15:52:12 by mweghofe          #+#    #+#             */
-/*   Updated: 2025/07/06 15:44:47 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/06 16:11:31 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/ft_printf.h"
 #include "launcher_bonus.h"
+#include "utils/colors_bonus.h"
 
 static const char	*result_as_string(t_result result)
 {
 	const char	*strings[] = {
-		"OK",
-		"KO",
-		"SIGSEGV",
-		"SIGBUS",
-		"UNDEFINED"
+		GREEN"OK"RESET,
+		YELLOW"KO"RESET,
+		RED"SIGSEGV"RESET,
+		RED"SIGBUS"RESET,
+		RED"UNDEFINED"RESET
 	};
 
 	return (strings[result]);
@@ -30,8 +31,8 @@ static const char	*result_as_string(t_result result)
 void	prt_test_result(const char *collection, const char *test_name,
 			t_result test_result)
 {
-	ft_printf("%s : %s : %s\n",
-		collection,
-		test_name,
-		result_as_string(test_result));
+	ft_printf("%s%s%s : %s%s%s : %s%s\n",
+		BOLD, collection, RESET,
+		ITALIC, test_name, RESET,
+		BOLD, result_as_string(test_result));
 }

bonus/framework/src/utils/colors_bonus.h
@@ -1,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors_bonus.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 15:58:58 by mweghofe          #+#    #+#             */
+/*   Updated: 2025/07/06 15:59:28 by mweghofe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define RESET "\033[0m"			// Reset all attributes
+#define BOLD "\033[1m"			// Bold / increased intensity
+#define DIM "\033[2m"			// Faint / decreased intensity
+#define ITALIC "\033[3m"		// Italic (not universally supported)
+#define UNDERLINE "\033[4m"		// Underline
+#define BLINK_SLOW "\033[5m"	// Blink(slow) (not universally supported)
+#define BLINK_FAST "\033[6m"	// Blink(rapid) (not universally supported)
+#define INVERSE "\033[7m"		// Swap foreground / background
+#define HIDDEN "\033[8m"		// Conceal / hidden text
+#define STRIKETHROUGH "\033[9m"	// Strike - through
+
+// 8 base ANSI colors
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"

bonus/framework/src/utils/prt_total_stats_bonus.c
@@ -1,27 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prt_total_stats_bonus_bonus.c                      :+:      :+:    :+:   */
+/*   prt_total_stats_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 17:30:32 by mweghofe          #+#    #+#             */
-/*   Updated: 2025/07/06 15:43:58 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/06 16:11:02 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/ft_printf.h"
 #include "t_libunit/t_libunit_bonus.h"
+#include "utils/colors_bonus.h"
 
 // prints all stats of t_stats, works with collection & total stats
 void	prt_total_stats(t_stats *stats, const char *name)
 {
-	ft_printf("\n%s :: total %i :: success %i :: fail %i :: crash %i\n\n",
+	char	*line_format;
+
+	if (ft_strcmp(name, "SUMMARY") != 0)
+		line_format = RESET DIM BOLD;
+	else
+		line_format = RESET BOLD;
+	ft_printf("\n%s%s :: total %i :: %ssuccess%s %i :: %sfail%s %i :: %scrash%s %i%s\n\n",
+		line_format,
 		name,
 		stats->n_tests,
+		GREEN, line_format, 
 		stats->n_success,
+		YELLOW, line_format,
 		stats->n_fail,
-		stats->n_crash);
+		RED, line_format,
+		stats->n_crash, RESET);
 }
 
 /*

commit 2fbe5e9dce92ec8d2ee4c5fa8e52f641692f925e

revert: Bring back file accidentily deleted in "feat: colored libunit output"
Caused by commit 7534be2aa2be2a2192b471fbf375ea83526b1927

framework/src/utils/prt_total_stats.c
@@ -1,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prt_total_stats.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 17:30:32 by mweghofe          #+#    #+#             */
+/*   Updated: 2025/07/05 21:20:59 by mweghofe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/ft_printf.h"
+#include "t_libunit/t_libunit.h"
+
+// prints all stats of t_stats, works with collection & total stats
+void	prt_total_stats(t_stats *stats, const char *name)
+{
+	ft_printf("\n%s :: total %i :: success %i :: fail %i :: crash %i\n\n",
+		name,
+		stats->n_tests,
+		stats->n_success,
+		stats->n_fail,
+		stats->n_crash);
+}
+
+/*
+
+	STRLEN : 01_sth : OK
+	STRLEN : 02_sth : KO
+	STRLEN : 03_sth : KO
+
+	STRLEN :: total tests 12 :: success 11 ...
+
+	SUMMARY :: total tests 235 :: success 200 ...
+
+*/
8bea7aa2206a2f5fa7ebbf07d83f7389529f2385
Lukas Dullinger <itislu.git@gmail.com>
2025-07-06 21:01:34 +0200

    fix: Clean any leftover tests on starting a new collection

--- bonus/framework/src/t_libunit/libunit_start_collection_bonus.c
+++ bonus/framework/src/t_libunit/libunit_start_collection_bonus.c
@@ -1,9 +1,12 @@
+#include "libft/libft.h"
 #include "t_libunit_bonus.h"
+#include "t_unit_test_bonus.h"
 
 void	libunit_start_collection(t_libunit *libunit, const char *name)
 {
 	if (libunit == NULL)
 		return ;
+	ft_lstclear(&libunit->tests, unit_test_free);
 	libunit->name = name;
 	libunit->max_name_len = 0;
 }

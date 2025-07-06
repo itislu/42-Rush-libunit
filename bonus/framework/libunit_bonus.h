f9f20e6ce2c88b7f8da468f4356cb4f94743e8c9
dltslt@42 <87982398+dltslt@users.noreply.github.com>
2025-07-06 23:22:15 +0200

    style: framework header

--- bonus/framework/libunit_bonus.h
+++ bonus/framework/libunit_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libunit_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mweghofe <mweghofe@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 10:13:27 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/06 16:55:56 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/06 23:22:04 by mweghofe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int			libunit_finish(t_libunit **libunit);
 void		libunit_free(t_libunit **libunit);
 bool		libunit_load(
 				t_libunit *libunit, const char *name, int (*func)(void));
-bool		libunit_load_stdout(t_libunit *libunit, const char *name, 
+bool		libunit_load_stdout(t_libunit *libunit, const char *name,
 				int (*func)(void), const char *expected_output);
 void		libunit_launch(t_libunit *libunit);
 t_libunit	*libunit_new(void);
 void		libunit_start_collection(t_libunit *libunit, const char *name);
-void		libunit_set_timeout(t_libunit *libunit, unsigned timeout);
+void		libunit_set_timeout(t_libunit *libunit, unsigned int timeout);
 
 #endif

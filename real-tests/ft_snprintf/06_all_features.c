37fa2e678668f8f3b62d5a8be361618d5a96efc2
Lukas Dullinger <itislu.git@gmail.com>
2025-07-10 10:41:43 +0200

    test: Add comparison of return value for `ft_snprintf` tests

--- real-tests/ft_snprintf/06_all_features.c
+++ real-tests/ft_snprintf/06_all_features.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 07:41:23 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/10 09:45:25 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/10 10:31:54 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,16 @@ int	all_features(void)
 {
 	char	ft_buffer[128];
 	char	og_buffer[128];
+	int		ft_ret;
+	int		og_ret;
 
-	ft_snprintf(ft_buffer, sizeof(ft_buffer), 
+	ft_ret = ft_snprintf(ft_buffer, sizeof(ft_buffer),
 		"%-2c %-4.2s-%-9p+%+01d#%- .0i.%-*.*u0%0#4x1%#-3.X%%*",
 		'.', "", NULL, INT_MIN, INT_MAX, 10, 20, UINT_MAX, UINT_MAX, 0);
-	snprintf(og_buffer, sizeof(og_buffer), 
+	og_ret = snprintf(og_buffer, sizeof(og_buffer), 
 		"%-2c %-4.2s-%-9p+%+01d#%- .0i.%-*.*u0%0#4x1%#-3.X%%*",
 		'.', "", NULL, INT_MIN, INT_MAX, 10, 20, UINT_MAX, UINT_MAX, 0);
-	if (strcmp(ft_buffer, og_buffer) == 0)
+	if (ft_ret == og_ret && strcmp(ft_buffer, og_buffer) == 0)
 		return (0);
 	return (-1);
 }

8cc6eeae5b0def815313160f6f164a5e48f9b5ce
Lukas Dullinger <itislu.git@gmail.com>
2025-07-10 10:19:12 +0200

    test: Compare `ft_printf` return value with `snprintf`

--- bonus/real-tests/ft_printf/02_stdout_width_bonus.c
+++ bonus/real-tests/ft_printf/02_stdout_width_bonus.c
@@ -6,15 +6,21 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 07:41:15 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/10 09:58:46 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/10 10:17:38 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/ft_printf.h"
+#include <stdio.h>
 
 int	stdout_width(void)
 {
-	if (ft_printf("%32s", "test_string") == 32)
+	int	ft_ret;
+	int	og_ret;
+
+	ft_ret = ft_printf("%32s", "test_string");
+	og_ret = snprintf(NULL, 0, "%32s", "test_string");
+	if (ft_ret == og_ret)
 		return (0);
 	return (-1);
 }
--- bonus/real-tests/ft_printf/06_stdout_all_features_bonus.c
+++ bonus/real-tests/ft_printf/06_stdout_all_features_bonus.c
@@ -6,17 +6,26 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 07:41:23 by ldulling          #+#    #+#             */
-/*   Updated: 2025/07/10 09:49:53 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/07/10 10:25:15 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/ft_printf.h"
 #include <limits.h>
+#include <stdio.h>
 
 int	stdout_all_features(void)
 {
-	if (ft_printf("%-2c %-4.2s-%-9p+%+01d#%- .0i.%-*.*u0%0#4x1%#-3.X%%*",
-		'.', "", NULL, INT_MIN, INT_MAX, 10, 20, UINT_MAX, UINT_MAX, 0) == 79)
+	int	ft_ret;
+	int	og_ret;
+
+	ft_ret = ft_printf(
+		"%-2c %-4.2s-%-9p+%+01d#%- .0i.%-*.*u0%0#4x1%#-3.X%%*",
+		'.', "", NULL, INT_MIN, INT_MAX, 10, 20, UINT_MAX, UINT_MAX, 0);
+	og_ret = snprintf(NULL, 0, 
+		"%-2c %-4.2s-%-9p+%+01d#%- .0i.%-*.*u0%0#4x1%#-3.X%%*",
+		'.', "", NULL, INT_MIN, INT_MAX, 10, 20, UINT_MAX, UINT_MAX, 0);
+	if (ft_ret == og_ret)
 		return (0);
 	return (-1);
 }
